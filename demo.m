27b2d0f91385d7199900c5655aa6c069bdb9add6
Oguzhan Ulucan <91013550+OguzhanUlucan@users.noreply.github.com>
2022-09-23 19:19:58 +0200

    Update demo.m

--- demo.m
+++ demo.m
@@ -23,6 +23,20 @@
 % this software for any purpose. It is provided "as is" without express
 % or implied warranty.
 %----------------------------------------------------------------------
+%
+% This work uses several packages provided by different authors;
+%
+% Pyramidal -> https://mericam.github.io/exposure_fusion/index.html
+%
+% Guided_Filter -> Kaiming He's website: http://kaiminghe.com/eccv10/
+%
+% signatureSal -> "Image Signature: Highlighting sparse salient regions", 
+% Xiaodi Hou, Jonathan Harel, and Christof Koch,
+% IEEE Trans. Pattern Anal. Mach. Intell. 34(1): 194-201 (2012)
+%
+% MEF-SSIM -> "Perceptual Quality Assessment for Multi-Exposure
+% Image Fusion",K. Ma et al., 
+% IEEE Transactions on Image Processing.
 
 clear all, close all, clc
 addpath('Pyramidal');
