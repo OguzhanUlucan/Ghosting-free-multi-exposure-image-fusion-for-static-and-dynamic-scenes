a8f25f2a3e317cf587022e8a9fb750370d926b4e
Oguzhan Ulucan <91013550+OguzhanUlucan@users.noreply.github.com>
2022-09-23 19:09:09 +0200

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,4 +1,28 @@
-% Oguzhan Ulucan & Diclehan Ulucan, M.Sc.
+% ========================================================================
+%
+% Corresponding Author
+% =========> M.Sc. Oguzhan Ulucan, Izmir, Turkey.
+% ============ oguzhan.ulucan.iz@gmail.com
+%
+% ========================================================================
+%
+% Ghosting-free multi-exposure image fusion for static and dynamic scenes
+%
+% Copyright(c) 2022 Oguzhan Ulucan, Diclehan Ulucan and Mehmet Turkan
+% 
+% All Rights Reserved.
+%
+% ----------------------------------------------------------------------
+% Permission to use, copy, or modify this software and its documentation
+% for educational and research purposes only and without fee is hereby
+% granted, provided that this copyright notice and the original authors'
+% names appear on all copies and supporting documentation. This program
+% shall not be used, rewritten, or adapted as the basis of a commercial
+% software or hardware product without first obtaining permission of the
+% authors. The authors make no representations about the suitability of
+% this software for any purpose. It is provided "as is" without express
+% or implied warranty.
+%----------------------------------------------------------------------
 
 clear all, close all, clc
 addpath('Pyramidal');
